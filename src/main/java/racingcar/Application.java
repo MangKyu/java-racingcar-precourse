68f90d5189ee30875201f89ca357bdd40ef1cf81
MangKyu <whalsrb1226@naver.com>
2022-04-20 22:37:50 +0900

    refactor: 메인클래스 객체 생성 부분을 별도의 메소드로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,8 +6,14 @@ import racingcar.game.Playable;
 import racingcar.player.Player;
 
 public class Application {
+
     public static void main(String[] args) {
-        final Playable game = new Game(new Player(), new GameView());
+        final Playable game = playable();
         game.play();
     }
+
+    private static Playable playable() {
+        return new Game(new Player(), new GameView());
+    }
+
 }
