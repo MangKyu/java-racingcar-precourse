182ecec7b8df81d57cbca32fe1914f3b6dadde26
MangKyu <whalsrb1226@naver.com>
2022-04-20 22:09:36 +0900

    feature: 게임이 정해진 라운드가 되면 게임을 종료한다.

--- src/main/java/racingcar/game/GameRound.java
+++ src/main/java/racingcar/game/GameRound.java
@@ -6,14 +6,21 @@ public class GameRound {
 
     private static final int MIN_ROUND = 1;
     private final int round;
+    private int current;
 
     public GameRound(final int round) {
         if (round < MIN_ROUND) {
             throw RacingCarException.invalidInput("Invalid round: " + round);
         }
         this.round = round;
+        this.current = 0;
     }
 
+    public boolean isEnd() {
+        return round == current;
+    }
 
-
+    public void start() {
+        this.current++;
+    }
 }
--- src/test/java/racingcar/game/GameRoundTest.java
+++ src/test/java/racingcar/game/GameRoundTest.java
@@ -1,16 +1,18 @@
 package racingcar.game;
 
 import org.assertj.core.api.AbstractThrowableAssert;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.errors.RacingCarException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameRoundTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, -1, -2, -3 -4})
+    @ValueSource(ints = {0, -1, -2, -3 - 4})
     void 이동할횟수입력성공_1보다작은입력(final int input) {
         // given
 
@@ -19,6 +21,40 @@ class GameRoundTest {
 
         // then
         result.isInstanceOf(IllegalArgumentException.class).hasMessageContaining(RacingCarException.PREFIX);
+    }
+
+    @Test
+    void 마지막라운드검사_맞음() {
+        // given
+        int totalRound = 3;
+        final GameRound round = new GameRound(totalRound);
+
+        for (int i = 0; i < totalRound; i++) {
+            round.start();
+        }
+
+        // when
+        final boolean result = round.isEnd();
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void 마지막라운드검사_아님() {
+        // given
+        int totalRound = 3;
+        final GameRound round = new GameRound(totalRound);
+
+        for (int i = 0; i < totalRound - 1; i++) {
+            round.start();
+        }
+
+        // when
+        final boolean result = round.isEnd();
+
+        // then
+        assertThat(result).isFalse();
     }
 
 }
